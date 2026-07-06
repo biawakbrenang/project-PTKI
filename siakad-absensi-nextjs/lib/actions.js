568a58524aa91a72fa0af652cbff194c05096238
biawakbrenang <113401465+biawakbrenang@users.noreply.github.com>
2026-07-06 12:40:23 +0700

    Delete siakad-absensi-nextjs directory

--- siakad-absensi-nextjs/lib/actions.js
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { redirect } from 'next/navigation';
-import { destroySession } from './auth';
-
-export async function logoutAction() {
-  destroySession();
-  redirect('/login');
-}
